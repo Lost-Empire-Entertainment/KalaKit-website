ad65e675324cdc68a156ef434a6687de26d52e02
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-06-09 00:40:03 +0300

    better button interaction

--- server/content/assets/js/main.js
+++ server/content/assets/js/main.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Admin panel loaded successfully.");
+
+    //
+    // ADJUST FOOTER POSITION
+    //
 
     const footer = document.querySelector(".footer");
 
@@ -16,4 +19,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
     adjustFooterPosition();
     window.addEventListener("resize", adjustFooterPosition);
+
+    //
+    // ROUTE/URL BUTTON CLICK
+    //
+
+    const navButtons = document.querySelectorAll(".footer-button[data-link]");
+
+    navButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const href = button.getAttribute("data-link");
+            const openNewTab = button.getAttribute("data-newtab") === "true";
+
+            if (href) {
+                if (openNewTab) {
+                    window.open(href, "_blank", "noopener,noreferrer");
+                } else {
+                    window.location.href = href;
+                }
+            }
+        });
+    });
 });
