4f0fa28d7a285d8c1325b17435081ebe205061d4
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-06-25 00:39:50 +0300

    setting up documentation for kalaserver

--- server/content/assets/js/sidebar-content/sidebar-content-kalaserver.js
+++ server/content/assets/js/sidebar-content/sidebar-content-kalaserver.js
@@ -1,8 +1,18 @@
 const sidebarItems = [
     { nest: 0, label: 'Manual', link: '#' },
     { nest: 1, label: 'placeholder', link: '#' },
+
     { nest: 0, label: 'API', link: '#' },
     { nest: 1, label: 'Client', link: '/kalaserver/client' },
+    { nest: 2, label: 'HandleClient', link: '/kalaserver/client/handleclient' },
+    { nest: 2, label: 'ExtractHeader', link: '/kalaserver/client/extractheader' },
+    { nest: 2, label: 'ExtractHeaderValue', link: '/kalaserver/client/extractheadervalue' },
+    { nest: 2, label: 'ParseByteRange', link: '/kalaserver/client/parsebyterange' },
+    { nest: 2, label: 'ConnectionStart', link: '/kalaserver/client/connectionstart' },
+    { nest: 2, label: 'CanConnect', link: '/kalaserver/client/canconnect' },
+    { nest: 2, label: 'IsValidRoute', link: '/kalaserver/client/isvalidroute' },
+    { nest: 2, label: 'SendEmail', link: '/kalaserver/client/sendemail' },
+    { nest: 2, label: 'SocketCleanup', link: '/kalaserver/client/socketcleanup' },
 ];
 
 function createSidebar() {
